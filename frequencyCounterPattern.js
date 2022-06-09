ee2ee837729fb95f9258ea7825af264ca3634118
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-06-09 21:37:45 +1200

    Add instructor provided solution

---
+++ frequencyCounterPattern.js
@@ -0,0 +1,38 @@
+// ===================
+// Frequency Counters
+// ===================
+
+// Pattern uses objects or sets to collect values/frequencies of values
+// Can often avoid need for nested loops or O(N^2) operations with arrays/strings
+// Comparing 2 or more inputs, checking anagrams, is value contained inside another value,
+// Avoid nested loops, 2 loops is way better than a nested loop (quadratic O(n))
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
+  }
+  return true
+}
+
+// Time Complexity: O(n)
+
+// Test cases
+same([1, 2, 3], [4, 1, 9]) // true
+same([1, 2, 3], [1, 9]) // false
+same([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
